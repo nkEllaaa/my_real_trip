5eafa7df27ea06ddbd2c110cbd52654191c6c563
nkEllaaa <dhrtns515@gmail.com>
2023-03-08 22:48:45 +0900

    slide, h2

--- js/card.js
+++ js/card.js
@@ -1,3 +1,7 @@
 document.querySelector('.btn-arrow-nk').addEventListener('click',function(){
-  document.querySelector('.swipe-wrap-nk').style.style.transform='translateX(-100vw)'
+  document.querySelector('.swipe-wrap-nk').style.transform='translateX(-100vw)'
+})
+
+document.querySelector('.btn-left-arrow-nk').addEventListener('click',function(){
+  document.querySelector('.swipe-wrap-nk').style.transform='translateX(0vw)'
 })
